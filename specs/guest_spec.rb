87947c6cb90e762e26abd8760a0a2889ac2ec97f
Fiona Wilson <fionaw237@gmail.com>
2018-07-27 16:37:27 +0100

    adds money parameter in guest class and tests

--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -5,11 +5,15 @@ require_relative("../guest")
 class GuestTest < MiniTest::Test
 
   def setup()
-    @guest = Guest.new("Fiona Wilson")
+    @guest = Guest.new("Fiona Wilson", 10)
   end
 
   def test_has_name()
     assert_equal("Fiona Wilson", @guest.name())
+  end
+
+  def test_has_money()
+    assert_equal(10, @guest.money())
   end
 
 end
