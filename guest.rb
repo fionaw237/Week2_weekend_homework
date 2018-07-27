2638251342a8b7d2897ad6e584f2982989866407
Fiona Wilson <fionaw237@gmail.com>
2018-07-27 19:30:57 +0100

    Adds favourite song parameter and getters for artist/title, also cheer method, and tests for all

--- guest.rb
+++ guest.rb
@@ -2,13 +2,28 @@ class Guest
 
   attr_reader :name, :money
 
-  def initialize(name, money)
+  def initialize(name, money, favourite_song)
     @name = name
     @money = money
+    @favourite_song = favourite_song
   end
 
   def pay_for_room(entry_fee)
     @money -= entry_fee
   end
+
+  def get_favourite_song_artist()
+    return @favourite_song[0]
+  end
+
+  def get_favourite_song_title()
+    return @favourite_song[1]
+  end
+
+  def cheer()
+    return "WOOHOO!"
+  end
+
+
 
 end
--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -5,7 +5,7 @@ require_relative("../guest")
 class GuestTest < MiniTest::Test
 
   def setup()
-    @guest = Guest.new("Fiona Wilson", 10)
+    @guest = Guest.new("Fiona Wilson", 10, ["Neck Deep", "Motion Sickness"])
   end
 
   def test_has_name()
@@ -16,9 +16,18 @@ class GuestTest < MiniTest::Test
     assert_equal(10, @guest.money())
   end
 
+  def test_has_favourite_song()
+    result = @guest.get_favourite_song_artist() + ", " + @guest.get_favourite_song_title()
+    assert_equal("Neck Deep, Motion Sickness", result)
+  end
+
   def test_can_pay_for_room()
     @guest.pay_for_room(5)
     assert_equal(5, @guest.money())
+  end
+
+  def test_can_cheer()
+    assert_equal("WOOHOO!", @guest.cheer())
   end
 
 end
